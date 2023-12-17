665338b806b21785aaec59da0ead85e49531aecd
c-Alvinn <141906500+c-Alvinn@users.noreply.github.com>
2023-12-17 19:15:26 -0300

    Updating Pedido Activity

--- app/src/main/java/com/example/trabalhofinalpdm/telas/PedidoActivity.kt
+++ app/src/main/java/com/example/trabalhofinalpdm/telas/PedidoActivity.kt
@@ -3,10 +3,15 @@ package com.example.trabalhofinalpdm.telas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.trabalhofinalpdm.R
+import com.example.trabalhofinalpdm.databinding.ActivityPedidoBinding
 
 class PedidoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPedidoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pedido)
+        binding = ActivityPedidoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
